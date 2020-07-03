2c9b70aadf9d171f37ddf46baf1af99c621af39a
Raphael Godel <lordservio@hotmail.com>
2020-07-03 13:18:44 -0300

    Initial commit

---
+++ src/main/java/com/apicondominio/ApiCondominioApplication.java
@@ -0,0 +1,13 @@
+package com.apicondominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCondominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCondominioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apicondominio/ApiCondominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apicondominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCondominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
